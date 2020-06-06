4e91a44a74a6f2351ca30c85d33b47371279114c
hyphop <art@khadas.com>
2020-06-06 20:25:24 +0900

    Sat Jun  6 20:25:24 +09 2020

--- files/openwrt_ext4.cmd
+++ files/openwrt_ext4.cmd
@@ -114,14 +114,15 @@ test "$ROOTFS" = "" -a "$devnum"  = "0:1" && ROOTFS=root=/dev/mmcblk0p2
 test "$ROOTFS" = "" -a "$devnum"  = "1:1" && ROOTFS=root=/dev/mmcblk1p2
 test "$ROOTFS" = "" -a "$devnum"  = "2:1" && ROOTFS=root=/dev/mmcblk2p2
 
-#test "$VENDOR_" != "" -a "$ROOTFS" = "root=/dev/mmcblk0p2" && ROOTFS=root=/dev/mmcblk2p2
 
-test "$Cdtb" = "$DTB_VIM1" -a "$ROOTFS" = "root=/dev/mmcblk0p2" && ROOTFS=root=/dev/mmcblk1p2
-test "$Cdtb" = "$DTB_VIM2" -a "$ROOTFS" = "root=/dev/mmcblk0p2" && ROOTFS=root=/dev/mmcblk1p2
-test "$Cdtb" = "$DTB_VIM3" -a "$ROOTFS" = "root=/dev/mmcblk0p2" && ROOTFS=root=/dev/mmcblk2p2
-test "$Cdtb" = "$DTB_VIM3L" -a "$ROOTFS" = "root=/dev/mmcblk0p2" && ROOTFS=root=/dev/mmcblk2p2
+echo "[i] openwrt rootfs $ROOTFS // DTB: $Cdtb"
 
-echo "[i] openwrt rootfs $ROOTFS"
+test "$VENDOR_" != "" -a "$ROOTFS" = "root=/dev/mmcblk0p2" && ROOTFS=root=/dev/mmcblk2p2
+
+test "$Cdtb" = "$DTB_VIM1" -a "$ROOTFS" = "root=/dev/mmcblk2p2" && ROOTFS=root=/dev/mmcblk1p2
+test "$Cdtb" = "$DTB_VIM2" -a "$ROOTFS" = "root=/dev/mmcblk2p2" && ROOTFS=root=/dev/mmcblk1p2
+
+echo "[i] openwrt rootfs $ROOTFS ((fixed))"
 
 ##############################################################
 
